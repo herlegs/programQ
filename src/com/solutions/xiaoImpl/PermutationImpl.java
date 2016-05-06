4694ca502b9ccbd98beaf7fdc745532f6230a105
Xiao <xiao_y@worksap.co.jp>
2016-05-06 16:01:32 +0800

    solve permutation

--- src/com/solutions/xiaoImpl/PermutationImpl.java
+++ src/com/solutions/xiaoImpl/PermutationImpl.java
@@ -4,6 +4,8 @@ import com.questions.Permutation;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.util.Util.*;
 
@@ -12,14 +14,44 @@ import static com.util.Util.*;
  */
 public class PermutationImpl implements Permutation{
     public static void main(String args[]){
+        char[] input;
+        Map<String, Integer> result = new HashMap<>();
+
+        input = new char[]{'么', '么', '哒'};
+        result.clear();
+        permutation(input, 0, input.length - 1, result);
+        p("---unique permutation---\n");
+        pResult(result);
 
     }
 
-    public static void permutation(char[] array){
-
+    public static void permutation(char[] array, int start, int end, Map<String, Integer> result){
+        if(start > end || start < 0 || end >= array.length){
+            return;
+        }
+        for(int i = start; i <= end; i++){
+            swap(array, start, i);
+            result.put(Arrays.toString(array), 0);
+            permutation(array, start + 1, end, result);
+            swap(array, start, i);
+        }
     }
 
-    public static void pCharArray(char[] array){
-        p(Arrays.toString(array));
+    private static void swap(char[] array, int i, int j){
+        char tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+    }
+
+    public static void pArray(char[] array){
+        p(Arrays.toString(array) + "\n");
+    }
+
+    public static void pResult(Map<String, Integer> result){
+        StringBuilder sb = new StringBuilder();
+        for(Map.Entry entry: result.entrySet()){
+            sb.append(entry.getKey() + "\n");
+        }
+        p(sb.toString());
     }
 }
