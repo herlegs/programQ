115366c09e09e3d9ec55e24833aab0452838866d
Xiao <xiao_y@worksap.co.jp>
2016-12-02 15:05:11 +0800

    commit

--- src/com/solutions/xiaoImpl/LRUCache.java
+++ src/com/solutions/xiaoImpl/LRUCache.java
@@ -1,19 +1,89 @@
 package com.solutions.xiaoImpl;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 /**
  * Created by xiao on 14/11/16.
  */
 public class LRUCache {
+    class Node{
+        int key;
+        int val;
+        Node prev;
+        Node next;
+        public Node(int key, int val){
+            this.key = key;
+            this.val = val;
+        }
+    }
+
+    Node head, tail;
+    Map<Integer, Node> cache;
+
+    private final int capacity;
+    private int size;
 
     public LRUCache(int capacity) {
-
+        this.capacity = capacity;
+        this.size = 0;
+        this.cache = new HashMap<>(size);
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head.next = tail;
+        tail.prev = head;
     }
 
     public int get(int key) {
-        return 0;
+        if(cache.containsKey(key)){
+            Node node = cache.get(key);
+            moveToFront(node);
+            return node.val;
+        }
+        return -1;
     }
 
     public void set(int key, int value) {
+        if(cache.containsKey(key)){
+            cache.get(key).val = value;
+            moveToFront(cache.get(key));
+        }
+        else{
+            Node node = new Node(key, value);
+            cache.put(key, node);
+            addToFront(node);
+            size++;
+            if(size > capacity){
+                Node last = tail.prev;
+                removeLast();
+                cache.remove(last.key);
+                size--;
+            }
+        }
+    }
 
+    private void removeLast(){
+        deleteNode(tail.prev);
+    }
+
+    private void moveToFront(Node node){
+        deleteNode(node);
+        addToFront(node);
+    }
+
+    private void addToFront(Node node){
+        Node next = head.next;
+        head.next = node;
+        next.prev = node;
+        node.prev = head;
+        node.next = next;
+    }
+
+    private void deleteNode(Node node){
+        Node prev = node.prev;
+        Node next = node.next;
+        prev.next = next;
+        next.prev = prev;
     }
 }
--- src/com/solutions/xiaoImpl/QuickSortImpl.java
+++ src/com/solutions/xiaoImpl/QuickSortImpl.java
@@ -10,8 +10,8 @@ import java.util.Map;
 /**
  * Created by xiao on 21/6/16.
  */
-public class QuickSortImpl implements QuickSort{
-    public static void main(String[] args){
+public class QuickSortImpl implements QuickSort {
+    public static void main(String[] args) {
         int[] array = null;
 //        array = new int[]{4,4,0,1,0,10,2,2};
 //        qsort(array, 0, array.length - 1);
@@ -21,35 +21,33 @@ public class QuickSortImpl implements QuickSort{
 //        qsort(array, 0, array.length - 1);
 //        System.out.println(Arrays.toString(array));
 
-        array = new int[]{};
+        array = new int[]{3,0,3,3,0,4,3};
         //qsort(array, 0, array.length - 1);
         //System.out.println(Arrays.toString(array));
 
 //        array = new int[]{9,8,7,6,5};
 //        qsort(array, 0, array.length - 1);
 //        System.out.println(Arrays.toString(array));
-        int[] result = mergesort(array, 0, array.length - 1);
-        System.out.println(Arrays.toString(result));
-        int[] a = new int[]{1,2};
-        Map<String, Integer> map = new HashMap<>();
+        qsort(array, 0, array.length - 1);
+        System.out.println(Arrays.toString(array));
     }
 
-    public static void qsort(int[] array, int start, int end){
-        if(start >= end || start < 0 || end >= array.length){
+    public static void qsort(int[] array, int start, int end) {
+        if (start >= end || start < 0 || end >= array.length) {
             return;
         }
-        if(end - start < 10){
-            insertionSort(array,start,end);
+        if (end - start < 10) {
+            insertionSort(array, start, end);
             return;
         }
         //int mid = (start+end)/2;
-        int mid = (start+end)/2;
+        int mid = (start + end) / 2;
         //int midVal = array[mid];
         int midVal = choosePivot(array[mid], array[start], array[end]);
         int i = start;
         int j = end;
         //why i <=j but not i < j (Rule 4)
-        while(i <= j){
+        while (i <= j) {
             //use < but not <= (Rule 1)
             //use <= need to check index out of bound:
             //maybe all the element in array is less than or equal to the pivot
@@ -59,66 +57,61 @@ public class QuickSortImpl implements QuickSort{
             //then for the right side 
 
             //pointer will stop at element equals or greater than the midVal
-            while(array[i] < midVal){
+            while (array[i] < midVal) {
                 i++;
             }
-            while(array[j] > midVal){
+            while (array[j] > midVal) {
                 j--;
             }
             //why if condition? (Rule 2)
             //
-            if(i <= j){
-                if(array[i] != array[j]){
+            if (i <= j) {
+                if (array[i] != array[j]) {
                     swap(array, i, j);
                 }
                 //why increment after swap (Rule 3)
                 //in case there two element with same value of pivot
                 i++;
                 j--;
-            }
-            else{
+            } else {
                 //System.out.println(i + "..." + j);
             }
         }
         //when i j meet at pivot value point then i - j = 2
         //otherwise i - j = 1
+        System.out.println("i  j: " + i + "   " + j);
         qsort(array, start, j);
         qsort(array, i, end);
     }
 
-    private static int choosePivot(int a, int b, int c){
-        if(a > b){
-            if(c > a){
+    private static int choosePivot(int a, int b, int c) {
+        if (a > b) {
+            if (c > a) {
                 return a;
-            }
-            else if(c > b){
+            } else if (c > b) {
                 return c;
-            }
-            else{
+            } else {
                 return b;
             }
-        }
-        else{
-            if(c > b){
+        } else {
+            if (c > b) {
                 return b;
-            }
-            else if(c > a){
+            } else if (c > a) {
                 return c;
-            }
-            else{
+            } else {
                 return a;
             }
         }
     }
 
-    private static void swap(int[] array, int i, int j){
+    private static void swap(int[] array, int i, int j) {
         int temp = array[i];
         array[i] = array[j];
         array[j] = temp;
     }
 
-    private static void insertionSort(int[] array, int start, int end){
-        if(start >= end || start < 0 || end >= array.length){
+    private static void insertionSort(int[] array, int start, int end) {
+        if (start >= end || start < 0 || end >= array.length) {
             return;
         }
         for (int i = start + 1; i <= end; i++) {
@@ -132,38 +125,37 @@ public class QuickSortImpl implements QuickSort{
 //                    break;
 //                }
 //            }
-            while(j >= 0 && array[j] > cur){
-                array[j+1] = array[j];
+            while (j >= 0 && array[j] > cur) {
+                array[j + 1] = array[j];
                 j--;
             }
-            array[j+1] = cur;
+            array[j + 1] = cur;
         }
     }
 
-    public static int[] mergesort(int[] array, int start, int end){
-        if(start > end){
+    public static int[] mergesort(int[] array, int start, int end) {
+        if (start > end) {
             return new int[0];
         }
-        if(start == end){
+        if (start == end) {
             return new int[]{array[start]};
         }
-        int mid = start + (end - start)/2;
+        int mid = start + (end - start) / 2;
         int[] left = mergesort(array, start, mid);
         int[] right = mergesort(array, mid + 1, end);
         return combine(left, right);
     }
 
-    private static int[] combine(int[] a, int[] b){
+    private static int[] combine(int[] a, int[] b) {
         int n = a.length;
         int m = b.length;
-        int[] c = new int[n+m];
+        int[] c = new int[n + m];
         int i = 0, j = 0, k = 0;
-        while(i < n && j < m){
-            if(a[i] < b[j]){
+        while (i < n && j < m) {
+            if (a[i] < b[j]) {
                 c[k] = a[i];
                 i++;
-            }
-            else{
+            } else {
                 c[k] = b[j];
                 j++;
             }
@@ -171,7 +163,7 @@ public class QuickSortImpl implements QuickSort{
         }
         int[] remained = (i == n) ? b : a;
         int index = (i == n) ? j : i;
-        while(k < n + m){
+        while (k < n + m) {
             c[k] = remained[index];
             index++;
             k++;
